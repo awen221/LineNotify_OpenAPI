23ce95b924555fbf30fe9a054440ff58db2fbe4d
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-14 17:59:35 +0800

    init

---
+++ LineNotify_OpenAPI/Controllers/api.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LineNotify_OpenAPI.Controllers
+{
+    using LineNotify.Interface.Func;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class api : ControllerBase
+        ,Iapi<IActionResult>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult notify(string token, string message)
+        {
+            var result = new LineNotify.api().notify(token, message);
+            return Ok(result);
+        }
+    }
+}
